820bf7f7a4a27e9197426c74004bd0b580bd92bb
Thomas Nabil <tomasnabil01@gmail.com>
2026-06-09 23:36:01 +0300

    Make terraform directory & modified the provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,23 @@
+terraform { 
+
+required_providers {
+  
+  aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+  }
+
+}
+
+backend "s3" {
+   
+   bucket = "e-commerce-terraform-state"
+   key    = "terraform.tfstate"
+   region = "us-east-1"
+
+}
+}
+
+provider "aws" {
+   region = var.region 
+}
